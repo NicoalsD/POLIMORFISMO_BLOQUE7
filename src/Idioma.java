c072b0020b32fe81ababddce0e1400ac8b714c7b
NicoalsD <22334455nico@gmail.com>
2026-04-08 20:58:14 -0500

    src

---
+++ src/Idioma.java
@@ -0,0 +1,7 @@
+
+
+public enum Idioma {
+    ESPAÑOL,
+    INGLES,
+    PORTUGUES
+}
---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+
+        String titulo;
+        double precio;
+        int anioPublicacion;
+        int numeroPaginas;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bienvenido a la editorial");
+        System.out.println("vamos a creaar un libro");
+        System.out.println("cual es el titulo del libro?");
+        titulo = input.nextLine();
+        System.out.println("cual es el precio del libro?");
+        precio = input.nextDouble();
+        System.out.println("cual es el año del libro?");
+        anioPublicacion = input.nextInt();
+        System.out.println("cual es el numero de paginas del libro?");
+        numeroPaginas = input.nextInt();
+
+        Libro miLibro=new Libro(titulo,precio,anioPublicacion,numeroPaginas);
+        System.out.println(miLibro);
+
+        input.close();
+
+    }
+}
